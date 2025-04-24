88522ebb88f74fb95aae1681691f083c82bcd0a2
xuewenwen <1295530887@qq.com>
2025-04-24 17:17:25 +0800

    在应用程序中添加主题管理器的环境对象，支持主题切换功能，并为主题菜单添加快捷键选项。

--- TimeTransTools/TimeTransToolsApp.swift
+++ TimeTransTools/TimeTransToolsApp.swift
@@ -9,9 +9,34 @@ import SwiftUI
 
 @main
 struct TimeTransToolsApp: App {
+    @StateObject private var themeManager = ThemeManager()
+    
     var body: some Scene {
         WindowGroup {
             MainMenuView()
+                .environmentObject(themeManager)
+                .preferredColorScheme(themeManager.appTheme.colorScheme())
+        }
+        .commands {
+            CommandMenu("主题") {
+                Button("跟随系统") {
+                    themeManager.setTheme(.system)
+                }
+                .keyboardShortcut("1", modifiers: [.command, .option])
+                .disabled(themeManager.appTheme == .system)
+                
+                Button("浅色模式") {
+                    themeManager.setTheme(.light)
+                }
+                .keyboardShortcut("2", modifiers: [.command, .option])
+                .disabled(themeManager.appTheme == .light)
+                
+                Button("深色模式") {
+                    themeManager.setTheme(.dark)
+                }
+                .keyboardShortcut("3", modifiers: [.command, .option])
+                .disabled(themeManager.appTheme == .dark)
+            }
         }
     }
 }
